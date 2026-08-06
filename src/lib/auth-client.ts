8f52cd653f74383fcbf501af8904f7ff2f422b69
IT Alfa Scorpii <chrisdiantodoni@gmail.com>
2026-08-06 18:16:31 +0700

    update

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,9 +3,11 @@ import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-	/** The base URL of the server (optional if you're using the same domain) */
-	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
-	plugins: [inferAdditionalFields<typeof auth>()],
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL: import.meta.env.VITE_BETTER_AUTH_URL || "http://localhost:3000",
+  plugins: [inferAdditionalFields<typeof auth>()],
 });
+
+console.log(import.meta.env.VITE_BETTER_AUTH_URL, "better auth");
 
 export const { signUp, signIn, signOut, useSession } = authClient;
