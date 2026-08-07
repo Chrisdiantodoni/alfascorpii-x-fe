59dab5d9de4fcd7378953ff17e9821faff0c1550
IT Alfa Scorpii <chrisdiantodoni@gmail.com>
2026-08-07 12:09:02 +0700

    update

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -182,7 +182,7 @@ export default function Topbar({
             aria-expanded={menuOpen}
             onClick={onMenuToggle}
             whileTap={{ scale: 0.9 }}
-            className="hidden items-center gap-[10px] max-lg:flex" // 👈 UBAH DI SINI
+            className="hidden items-center gap-[10px] lg:hidden!" // 👈 UBAH DI SINI
           >
             {/*<span
               className={`hidden sm:inline-block text-right w-[56px] text-[11px] font-semibold tracking-[0.2em] transition-colors uppercase ${
