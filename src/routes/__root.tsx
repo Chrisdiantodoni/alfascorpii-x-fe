ca3e265569eb72d35fc71d7c5e817fabdff726da
IT Alfa Scorpii <chrisdiantodoni@gmail.com>
2026-08-11 11:34:22 +0700

    update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -41,6 +41,8 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
         <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
         <script dangerouslySetInnerHTML={{ __html: SPLASH_SEEN_SCRIPT }} />
+        {/* Pindahkan splash script ke head agar dieksekusi sebelum render */}
+        <script dangerouslySetInnerHTML={{ __html: SPLASH_SCRIPT }} />
         {/*<link rel="stylesheet" href={appCss} />*/}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
