9f330d60e9c841863d28195624c075abab2fe1f8
IT Alfa Scorpii <chrisdiantodoni@gmail.com>
2026-08-20 21:46:11 +0700

    update

--- src/routes/_public/product/$slug/index.tsx
+++ src/routes/_public/product/$slug/index.tsx
@@ -168,7 +168,7 @@ function ProductDetail() {
   };
 
   return (
-    <div className={banners.hero ? "pt-24" : ""}>
+    <div className={banners.hero ? "pt-[6.75rem]" : ""}>
       <PageBanner hero={banners.hero} top={banners.top} />
 
       <section className="pt-32 pb-8">
